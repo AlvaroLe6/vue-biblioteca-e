34779b32bd686f189e449bf7e8ddd8141b9761f4
Alvarole6 <alvaro_147@hotmail.es>
2023-12-08 23:15:27 +0100

    08/12/2023

---
+++ src/components/Libro.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+
+    libro:{
+        type:Object
+    }
+})
+</script>
+
+<template>
+    <v-col
+    cols="12"
+    md="4"
+    >
+    <v-card>
+
+        <v-img :src="libro.imagen" height="200" cover>
+        <v-card-title>
+{{ libro.titulo }}
+        </v-card-title>  
+        <v-card-title>
+{{ libro.Autor }}
+        </v-card-title>      
+        </v-img>
+    </v-card>
+
+        <h2>Libro...</h2>
+    </v-col>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,25 @@
 <script setup>
+
+import  useLibros from '../composables/useLibros'
+import Libro from '../components/Libro.vue'
+
+const {librosCollection } = useLibros()
 </script>
 
 <template>
   <main>
    <h1 class="text-center text-h3 font-weight-bold my-5"> Biblioteca</h1>
+  <v-card flat class ="py-10">
+
+
+      <v-row>
+        <Libro
+        v-for = "libro in librosCollection"
+        :key="libro.id"
+        :libro ="libro"
+        />
+      </v-row>
+  </v-card>
+  
   </main>
 </template>
