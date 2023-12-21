a12a146edd3cf5b40178e398e1f37f6d9ba2ea2b
AlvaroLe6 <118232007+AlvaroLe6@users.noreply.github.com>
2023-12-21 17:08:49 -0400

    21/12/2023

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,89 +1,113 @@
 <script setup>
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import useLibros from "../composables/useLibros";
+import Libro from "../components/Libro.vue";
 
-import  useLibros from '../composables/useLibros'
-import Libro from '../components/Libro.vue'
+const { areasSeleccionadas, librosFiltradosPorArea, librosCollection } = useLibros();
 
-const { areasSeleccionadas, librosFiltradosPorArea } = useLibros(); // Obtiene los resultados de useLibros
+const areas = [
+  "Ingenieria",
+  "Financiera",
+  "Administrativa",
+  "Legal",
+  "Educacion",
+  "Diseño",
+  "Tecnologia",
+  "Salud",
+  "Empresarial",
+  "Social",
+];
 
-/* Variables para manejar el estado de los checkboxes
-const areaSalud = computed(() => areasSeleccionadas.value.includes('Salud'));
-const areaEmpresarial = computed(() => areasSeleccionadas.value.includes('Empresarial'));
-const areaLegal = computed(() => areasSeleccionadas.value.includes('Legal'));
-*/
 
-// Variables reactivas para manejar el estado de los checkboxes
-const areaSalud = ref(false);
-const areaEmpresarial = ref(false);
-const areaLegal = ref(false);
+// Modelos para el v-combobox
+const model = ref([]);
 
+// Propiedad de búsqueda para el v-combobox
+const search = ref(null);
 
-// Funciones para manejar cambios en las áreas seleccionadas
-// Función para manejar cambios en las áreas seleccionadas
-const toggleArea = (area) => {
-  const index = areasSeleccionadas.value.indexOf(area);
-  if (index !== -1) {
-    areasSeleccionadas.value.splice(index, 1); // Eliminar el área si ya está seleccionada
-    if (area === 'Salud') {
-      areaSalud.value = false;
-    } else if (area === 'Empresarial') {
-      areaEmpresarial.value = false;
-    } else if (area === 'Legal') {
-      areaLegal.value = false;
-    }
-  } else {
-    areasSeleccionadas.value.push(area); // Agregar el área si no está seleccionada
-    if (area === 'Salud') {
-      areaSalud.value = true;
-    } else if (area === 'Empresarial') {
-      areaEmpresarial.value = true;
-    } else if (area === 'Legal') {
-      areaLegal.value = true;
-    }
+// Filtrar libros según áreas seleccionadas
+const filteredLibros = computed(() => {
+  const libros = librosCollection.value;
+  const selectedAreas = areasSeleccionadas.value;
+
+  if (!libros || libros.length === 0 || selectedAreas.length === 0) {
+    return libros; // Si no hay áreas seleccionadas, mostrar todos los libros
   }
-};
 
-// Observar cambios en las áreas seleccionadas para actualizar el estado de los checkboxes
-areasSeleccionadas.value.forEach(area => {
-  if (area === 'Salud') {
-    areaSalud = true;
-  } else if (area === 'Empresarial') {
-    areaEmpresarial = true;
-  } else if (area === 'Legal') {
-    areaLegal = true;
-  }
+  return libros.filter(libro => selectedAreas.includes(libro.area));
 });
-
 </script>
 
 <template>
   <main>
-   <h1 class="text-center text-h3 font-weight-bold my-5"> Biblioteca</h1>
-  <v-card flat class ="py-10">
-
-<v-card-title class="text-h5">
-  Búsqueda
-  <v-row>
-          <v-col cols="4">
-            <v-checkbox label="Salud" v-model="areaSalud" @change="toggleArea('Salud')" class="mr-4"/>
-          </v-col>
-          <v-col cols="4">
-            <v-checkbox label="Empresarial" v-model="areaEmpresarial"  @change="toggleArea('Empresarial')" class="mr-4"/>
-          </v-col>
-          <v-col cols="4">
-            <v-checkbox label="Legal" v-model="areaLegal" @change="toggleArea('Legal')" class="mr-4"/>
-          </v-col>
+    <h1 class="text-center text-h3 font-weight-bold my-5">Biblioteca</h1>
+    <v-card flat class="py-10">
+      <v-card-title class="text-h5">
+        Búsqueda
+        <v-row>
+          <v-container fluid>
+            <v-combobox
+              v-model="model"
+              v-model:search="search"
+              :hide-no-data="false"
+              :items="items"
+              hide-selected
+              hint="Máximo de 5 etiquetas"
+              label="Agrega algunas etiquetas"
+              multiple
+              persistent-hint
+              small-chips
+            >
+              <template v-slot:no-data>
+                <v-list-item>
+                  <v-list-item-title>
+                    No results matching "<strong>{{ search }}</strong
+                    >". Press <kbd>enter</kbd> to create a new one
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-combobox>
+          </v-container>
         </v-row>
-</v-card-title>
+      </v-card-title>
       <v-row>
         <Libro
-        v-for = "libro in /*librosFiltradosPorArea*/ areasSeleccionadas"
-        :key="libro.id"
-        :libro ="libro"
+          v-for="libro in filteredLibros"
+          :key="libro.id"
+          :libro="libro"
         />
       </v-row>
-  </v-card>
-  
+    </v-card>
   </main>
 </template>
+<script>
+export default {
+  data: () => ({
+    items: [
+      "Ingenieria",
+      "Financiera",
+      "Administrativa",
+      "Legal",
+      "Educacion",
+      "Diseño",
+      "Tecnologia",
+      "Salud",
+      "Empresarial",
+      "Social",
+    ],
+    model: ["Seleccionar un área"],
+    search: null,
+  }),
+
+  watch: {
+    model(val) {
+      if (val.length > 5) {
+        this.$nextTick(() => this.model.pop());
+      }
+    },
+  },
+};
+
+
+</script>
